27292484d4db998160678cf7822e3f66b2efc4de
Logicist-infosec <74598629+Logicist-infosec@users.noreply.github.com>
2020-12-17 19:09:46 +0100

    Update mac_changer.py

--- MAC Change/mac_changer.py
+++ MAC Change/mac_changer.py
@@ -21,7 +21,7 @@ def get_arguments():
     # new_mac = "00:11:22:33:44:77"
 
     # Alternative Two
-    # By input. Not efficient if there are many options, and users only want to add some of them. 
+    # By input. Not efficient if there are many options and users only want to add some of them. 
     # interface = input("interface > ")
     # new_mac = input("New MAC address > ")
 
